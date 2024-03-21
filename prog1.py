e278bf43c5dbc8f5b3b5f02ff9a633cbacf66d42
nnma24 <163966579+nnma24@users.noreply.github.com>
2024-03-21 14:05:39 +0900

    Add files via upload

--- prog1.py
+++ prog1.py
@@ -1,4 +1,39 @@
 import numpy as np
 
-arrayNumber = np.random.randint(0, 1000000, size=20)
+arrayNumber = np.random.randint(0, 10, size=20)
 print(arrayNumber)
+
+# to make a group of unique numbers
+# A = [5 7 6 7 6 0 2 6 6 8 7 4 7 5 4 1 2 9 6 0]
+# unique_A = [5, 7, 6, 0, 2, 8, 4, 1, 2, 9]
+
+uniqueList = []
+for number in arrayNumber:
+    if number in uniqueList:
+        pass
+    else:
+        uniqueList.append(number)
+#
+print("unique: ", uniqueList)
+
+uniqueList2 = list() # []
+for number in arrayNumber:
+    if number not in uniqueList2:
+        uniqueList2.append(number)
+#
+print("unique2: ", uniqueList2)
+
+# uniqueList3 = [number for number in arrayNumber if number not in uniqueList3] 
+
+myList = []
+myList2 = list()
+
+# set, dict
+uniqueSet = set ()
+for num in arrayNumber:
+    uniqueSet.add(num)
+#
+print(uniqueSet)
+for elem in uniqueSet:
+    print(elem, end=' ')
+print()
