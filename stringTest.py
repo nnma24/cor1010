6d149136770d699034aeb9e8e38b24a1cb729040
nnma24 <163966579+nnma24@users.noreply.github.com>
2024-03-21 14:42:42 +0900

    Add files via upload

---
+++ stringTest.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+longString = "Oh, well! it is just as he chooses. Nobody wants him to come."
+# ; though i shall always say that he used my daughter extremely ill; and, if I was
+# her, I would not have put up with it. Well, my comfort is, I am sure
+# Jane will die of a broken heart, and then he will be sorry for what he
+# has done."
+
+print( len(longString) )
+
+# compute the number of characters in longString
